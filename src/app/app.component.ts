337c63029b4cfb9267484603dbbb268d92d04b83
deepblue597 <iasonas2001@hotmail.com>
2022-04-12 21:50:54 +0300

    formbuild creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import UsersJson from './users.json';
 import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators, FormArray, FormControl} from '@angular/forms';
+import { forbiddenNameValidator } from './shared/user-name.validator';
+import { PasswordValidator } from './shared/password.validator';
+//import { keys } from 'ts-transformer-keys';
 
 
 /* this is  an interface to receive the data from the json file */
@@ -43,23 +46,103 @@ export class AppComponent {
   notifications = 1; 
   displayedColumns: string[] = ['position' ]; // 'name', 'weight', 'symbol'
   dataSource = ELEMENT_DATA;
+
+  /* ------------------------------------ */ 
+  /* angular forms tutorial */ 
+
+ 
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('Jason'), 
+  //   password: new FormControl(''), 
+  //   confirmPassword: new FormControl(''),  
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl(''),
+  //   })
+  // }) ; 
+
+  /* formbuilder is a simpler way to create form groups */
+
+  // userformgroup = this.fb.group<USERS>({
+  //   id: [1], 
+  //   name: ['jason'], 
+  //   username: ['deepblue'], 
+  //   email: ['deepblue@mail.com']
+  // }) 
+  
+  registrationForm = this.fb.group({
+    userName: ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator]], 
+    email: [''], 
+    password: [''], 
+    confirmPassword: [''], 
+    address: this.fb.group({
+      city: [''], 
+      state: [''], 
+      postalCode: ['']  
+    }), 
+    alternateEmails: this.fb.array([])   
+  }, {validator: PasswordValidator}) ; 
+
+
+  get alternateEmails() {
+    return this.registrationForm.get('alternateEmails') as FormArray ; 
+  }
+
+  addAlternateEmail() {
+    this.alternateEmails.push(this.fb.control('')) ; 
+  }
+
+  get userName(){
+    return this.registrationForm.get('userName') ; 
+  }
+
+  /* setting the values with dummy values 
+  if we want to fill all the values we use setValue 
+  otherwise we use patchValue */ 
+  loadApiData() 
+  {
+    this.registrationForm.patchValue({
+      userName: 'Ioanna', 
+      password: '12345', 
+      confirmPassword: '12345',  
+      address: {
+        city: 'heraklion',
+        state: 'crete',
+        
+      }
+    }) ;
+  }   
+
+
+ /* testing */ 
+
+
+  /* ----------------------------------- */
+
+  /* the next 4 are needed for the stepper */ 
+  /* the next 4 are needed for the stepper */
+  groups: FormGroup[] = []; 
   secondFormGroup!: FormGroup; 
   thirdFormGroup!: FormGroup; 
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor(private _formBuilder: FormBuilder, private fb: FormBuilder)  {} //the fb is for the form tutorial 
   pickedCommandName: String = '' ; 
   
   Users: USERS[] = UsersJson;  //we use this for the demonstration (no need for the names string)
-  
+  properties: number = Object.keys(this.Users[1]).length ; 
 
   Names:String[] = [] ;
   options: string[] = ['Angular', 'React' , 'View'] ; 
   ngOnInit(){
+    console.log(this.registrationForm) ; 
     for(let i=0 ; i<this.Users.length; i++) 
     {
       this.Names[i] = this.Users[i].name;
       
     }
     console.log(this.Names) ;   //logs the names 
+    /* this is for the stepper also */ 
+
     this.secondFormGroup = this._formBuilder.group({
       secondCtrl: ['', Validators.required],
       
@@ -69,6 +152,7 @@ export class AppComponent {
       
     });
     console.log(this.secondFormGroup) ; 
+    console.log('the number of objects are ' + Object.keys(this.Users[1]).length) ; //number of porperties of  a user type object 
 
   }
 
@@ -103,15 +187,16 @@ export class AppComponent {
     this.secondCompleted = false ; 
   }
 
-
+ 
 
   onClick(name: any): void {
      
     this.pickedCommandName = name ; 
     console.log(name) ;  //consoles the name that has been chosen
-    this.firstComplete() ;  
+    this.firstComplete() ; // calls the first complete function  
     console.log(this.firstCompleted) ;  
     console.log(this.pickedCommandName) ;  
+    
      
   }
 
